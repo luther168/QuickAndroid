23844470bbedf71d5feea9564ae42cb0fd124cbb
Hurston <1554140485@qq.com>
2018-04-02 23:54:02 +0800

    Create the necessary modules including App and QuickLibrary

---
+++ App/src/main/java/cn/luo/android/quick/SplashActivity.java
@@ -0,0 +1,13 @@
+package cn.luo.android.quick;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
